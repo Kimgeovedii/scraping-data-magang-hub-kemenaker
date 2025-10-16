7233be452ce92f1dbcaf44995ba5b708aa27d908
Kimgeovedii <mohamadmustofahakim@gmail.com>
2025-10-16 15:36:43 +0700

    `Update README.MD and config.py to use .env file for environment variables`

--- config.py
+++ config.py
@@ -1,14 +1,20 @@
-BASE_URL = "https://maganghub.kemnaker.go.id/be/v1/api/"
+import os
+from dotenv import load_dotenv
 
-# Kosongkan bagian token, isi manual sebelum menjalankan
-HEADERS = {
-    'accept': 'application/json',
-    'accept-language': 'id,en-US;q=0.9,en;q=0.8',
-    'authorization': '',
-    'Cookie': ''
+load_dotenv()
+BASE_URL = os.getenv("BASE_URL")
+MASTER_LIST_ENDPOINT = os.getenv("MASTER_LIST_ENDPOINT")
+AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+COOKIE_TOKEN = os.getenv("COOKIE_TOKEN")
+
+HEADERS =  {
+  'accept': 'application/json',
+  'accept-language': 'id,en-US;q=0.9,en;q=0.8',
+  'authorization': AUTH_TOKEN,
+  'Cookie': COOKIE_TOKEN
 }
 
-MASTER_LIST_ENDPOINT = ""  # isi dengan endpoint program-peserta
+MASTER_LIST_ENDPOINT = MASTER_LIST_ENDPOINT
 DETAIL_ENDPOINTS = {
     'user_info': 'crud-users',
     'pendidikan': 'portofolio-pendidikan',
